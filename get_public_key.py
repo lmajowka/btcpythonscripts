7b37c1842f543e837ca1fdb3d44333d0efb77067
Denzy <108025321+denzylegacy@users.noreply.github.com>
2024-07-29 15:46:40 -0300

    Initial algorithm improvement
    Otimização da função principal, adição de tratamento de erros e docstring.

--- get_public_key.py
+++ get_public_key.py
@@ -1,22 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
+from typing import Optional, List, Dict, Any
 
-def get_scriptpubkey(carteira):
-    url = f"https://mempool.space/api/address/{carteira}/txs/mempool"
-    response = requests.get(url)
+
+BASE_URL = "https://mempool.space/api"
+
+
+def get_scriptpubkey(wallet: str) -> str:
+    """
+    Procura a chave pública de uma carteira.
+
+    Args:
+        wallet (str): O endereço da carteira Bitcoin.
+
+    Returns:
+        str: O scriptpubkey ou uma mensagem de erro.
+    """
     
-    if response.status_code == 200:
-        transactions = response.json()
-        for transaction in transactions:
-            for vout in transaction['vout']:
-                if vout['scriptpubkey_address'] == carteira:
-                    scriptpubkey = vout['scriptpubkey']
-                    # Remove os 4 primeiros caracteres
-                    return scriptpubkey[4:]
-        return "Nenhuma transacao encontrada para a carteira especificada."
-    else:
-        return f"Erro na requisicao: {response.status_code}"
+    if not wallet:
+        return "Endereço de carteira inválido!"
 
-# Exemplo de uso
-carteira = "bc1qlvl0qnk8x2xgtcdsgdzu7a66k7h993cfzg4xfu"
-scriptpubkey = get_scriptpubkey(carteira)
-print(f"ScriptPubKey: {scriptpubkey[4:]}")
+    url: str = f"{BASE_URL}/address/{wallet}/txs/mempool"
+
+    try:
+        response: requests.Response = requests.get(url)
+        response.raise_for_status()
+        
+        transactions: List[Dict[str, Any]] = response.json()
+        scriptpubkey: Optional[str] = next(
+            (vout["scriptpubkey"][4:]
+             for tx in transactions
+             for vout in tx["vout"]
+             if vout.get("scriptpubkey_address") == wallet),
+            None
+        )
+
+        if scriptpubkey:
+            return f"[+] CHAVE PÚBLICA: {scriptpubkey}\n"
+        return "Nenhuma transação encontrada para a carteira especificada."
+    except requests.RequestException as e:
+        return f"Erro na requisição: {str(e)}"
+    except ValueError:
+        return "Erro ao processar a resposta JSON."
+
+
+if __name__ == "__main__":
+    print("Exemplo de Endereço: bc1qlqlch88awgah90y8890rtaqdf867las9kk9q2h")
+
+    wallet: str = input("\nInforme o endereço da carteira: ")
+
+    scriptpubkey: str = get_scriptpubkey(wallet)
+    print(f"\n{scriptpubkey}")
